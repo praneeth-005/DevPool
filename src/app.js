db29a78a9f63d2ab2111b8dc456c68f3f3cc28ee
praneeth <surapureddy.praneeth@gmail.com>
2025-06-18 16:28:27 +0530

    practicing routes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.use("/test", (req, res) => {
-    res.send("Hello!");
+app.use("/user", (req, res)=>{
+    res.send("Hello All!")
+})
+
+app.use("/about", (req, res) => {
+    res.send("About Page!");
 });
 
-app.use((req, res) => {
-    res.send("Welcome to server world!");
+app.get("/user", (req, res)=>{
+    res.send({firstName: "Praneeth", lastName: "Surapureddy"});
 });
+
+app.post("/user", (req, res)=>{
+    res.send("Data successfully saved to DB.");
+});
+
+
 
 app.listen("3000", () => {
     console.log("Server is succesfully launched and listening for request on port 3000.");
